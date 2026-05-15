f4039bf213e715b1d3b9cb8056efece8ab17d4a0
hangsa <lihailong0510@163.com>
2026-05-15 11:40:53 +0800

    feat: add Alt+E keyboard shortcut support

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -40,3 +40,14 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   sendResponse();
   return true;
 });
+
+chrome.commands.onCommand.addListener(async (command) => {
+  if (command === 'toggle-selection') {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    try {
+      await chrome.tabs.sendMessage(tab.id, { type: 'TOGGLE_SELECTION' });
+    } catch (err) {
+      // Page may not have content script loaded
+    }
+  }
+});
